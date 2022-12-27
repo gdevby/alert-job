5100ca9944d7e42131bfa0d15d14018f8a29e5d6
alexander <alexander@alexander-desktop>
2022-12-27 15:08:42 +0300

    changes made

--- notification-alert-job/src/main/java/by/gdev/alert/job/notification/controller/MailController.java
+++ notification-alert-job/src/main/java/by/gdev/alert/job/notification/controller/MailController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import by.gdev.alert.job.notification.model.UserMail;
 import by.gdev.alert.job.notification.service.MailService;
+import by.gdev.common.model.UserNotification;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
@@ -19,12 +19,12 @@ public class MailController {
 	private final MailService service;
 
 	@PostMapping("mail")
-	public ResponseEntity<Mono<Void>> mailMessage(@RequestBody UserMail userMail) {
+	public ResponseEntity<Mono<Void>> mailMessage(@RequestBody UserNotification userMail) {
 		return ResponseEntity.ok(service.sendMessage(userMail));
 	}
 
 	@PostMapping("telegram")
-	public ResponseEntity<Mono<Void>> sendTelegram(@RequestBody UserMail userMail) {
+	public ResponseEntity<Mono<Void>> sendTelegram(@RequestBody UserNotification userMail) {
 		return ResponseEntity.ok(service.sendMessageToTelegram(userMail));
 	}
 }
