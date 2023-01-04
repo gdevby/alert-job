e2c65b2d2b4e00d9302f733ac673b5dcad538b27
daniel <daniel@daniel-desktop>
2023-01-04 14:58:16 +0300

    changed props name

--- front/src/components/field/Field.js
+++ front/src/components/field/Field.js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react'
 
 
-const Field = ({ type, placeholder, onChange }) => {
+const Field = ({ type, placeholder, cb }) => {
 	const [value, setValue] = useState('')
 	
 	const handleValue = event => {
 		const input_text = event.target.value
 		setValue(input_text)
-		onChange(input_text)
+		cb(input_text)
 	}
 	
 	return <input type={type} placeholder={placeholder} onChange={handleValue} value={value}/>
