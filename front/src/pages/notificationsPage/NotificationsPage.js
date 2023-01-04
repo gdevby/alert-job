66a233a33d28a2979322d2604f0a78205539fce5
daniel <daniel@daniel-desktop>
2023-01-04 14:58:30 +0300

    added api for test alert

--- front/src/pages/notificationsPage/NotificationsPage.js
+++ front/src/pages/notificationsPage/NotificationsPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 
 import Title from '../../components/title/Title'
 import Field from '../../components/field/Field'
@@ -12,26 +12,43 @@ import './notificationsPage.scss'
 const NotificationsPage = () => {
 	const [platforms, setPlatforms] = useState([{ name: 'email', id: 1 }, { name: 'telegram', id: 2 }])
 	const [currentPlatform, setCurrentPlatform] = useState({})
+	const [alertStatus, setAlertStatus] = useState(false)
+	const [alertType, setAlertType] = useState('')
 
 
 	const sendTestNotification = () => {
 		coreService.sendTestMessage().then(console.log)
-
+	}
+	
+	const handleCurrentPlatform = (data) => {
+		console.log(data)
+		setCurrentPlatform(data)
+		coreService.changeAlertsType(data.name == 'email')
+	}
+	
+	const handleAlertsStatus = e => {
+		coreService.changeAlertsStatus(!alertStatus).then(() => setAlertStatus(!alertStatus))
+		
 	}
 
 	useEffect(() => {
-		coreService.getStatue().then(console.log)
+		coreService.getStatue().then(response => setAlertStatus(response.data))
 	}, [])
+	
 
 	return <div className='notification_page'>
 		<div className='container'>
 			<Title text='Настройка уведомлений' />
 			<div className='notification_source'>
-				<DropDownList open={false} defaultValue={'email'} elems={platforms} cb={setCurrentPlatform} />
-				<Field type='text' placeholder='Введите адрес' />
+				<DropDownList open={false} defaultValue={'email'} elems={platforms} cb={handleCurrentPlatform} />
+				<Field type='text' placeholder='Введите адрес' cb={setAlertType}/>
 				<div className='notification_source__send-btn'>
 					<Button text={'Отправить тестовое уведомление'} onClick={sendTestNotification} />
 				</div>
+			</div>
+			<div>
+				<input type='checkbox' onChange={handleAlertsStatus} checked={alertStatus}/>
+				Отключить
 			</div>
 
 
--- front/src/services/parser/endponits/coreService.js
+++ front/src/services/parser/endponits/coreService.js
@@ -4,7 +4,9 @@ const coreService = {
 	checkAuth: () => api.get('user/test'),
 	checkAuth1: () => api.post('user/authentication'),
 	sendTestMessage: () => api.post('test-message'),
-	getStatue: () => api.get('user/alerts')
+	getStatue: () => api.get('user/alerts'),
+	changeAlertsStatus: (status) => api.patch(`user/alerts?status=${status}`),
+	changeAlertsType: (default_send) => api.patch(`user/alerts/type?default_send=${default_send}`)
 }
 
 export { coreService }
