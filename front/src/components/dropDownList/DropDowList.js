46a54c2fbd02ec415506a17a7b1951ee5ff45b00
daniel <daniel@daniel-desktop>
2023-01-05 15:30:56 +0300

    improved view

--- front/src/components/dropDownList/DropDowList.js
+++ front/src/components/dropDownList/DropDowList.js
@@ -1,26 +1,33 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Button from '../button/Button'
 
 import './dropDownList.scss'
 
-const DropDownList = ({open = false, defaultValue, elems, cb}) => {
+const DropDownList = ({ open = false, defaultValue, elems, cb }) => {
 	const [isOpen, setIsOpen] = useState(open)
 	const [value, setValue] = useState(defaultValue)
-	
-	
+
+
 	const handleValue = event => {
 		setValue(event.target.textContent)
-		cb({name: event.target.textContent, id: event.target.id})
+		cb({ name: event.target.textContent, id: event.target.id })
 		setIsOpen(false)
 	}
-	
+
+	useEffect(() => {
+		setValue(defaultValue)
+	}, [defaultValue])
+
 	const handleOpen = () => {
-		setIsOpen(true)
+		setIsOpen(!isOpen)
 	}
-	
+
 	return <div className='list'>
-		<Button text={value} onClick={handleOpen} />
+		<div className='list__button' onClick={handleOpen} >
+			{value} 
+			<i className={isOpen ? 'arrow arrow-up' : 'arrow arrow-down'}></i>
+		</div>
 		{isOpen && <div className='list_items'>
 			{elems.map((item, index) => <div key={index} id={item.id} onClick={handleValue} className='list_item'>{item.name}</div>)}
 		</div>}
