29827f46be294b1c9d9d0ff96e3dbc4f8d44f706
daniel <daniel@daniel-desktop>
2023-01-06 15:22:30 +0300

    added hook

---
+++ front/src/hooks/use-debounce.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+
+export default function useDebounce(value, delay) {
+	const [debouncedValue, setDebounceValue] = useState(value)
+	
+	
+	useEffect(() => {
+		const handler = setTimeout(() => {
+			setDebounceValue(value)
+		}, delay)
+		
+		
+		return () => {
+			clearTimeout(handler)
+		}
+	}, [value])
+	
+	
+	return debouncedValue
+
+}
