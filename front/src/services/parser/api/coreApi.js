ad67aef9aeeff8be404cd519dd6e9f66ec36e7dd
Daniel <jasp_m_lv@mail.ru>
2023-01-12 17:45:29 +0300

    changed api

--- front/src/services/parser/api/coreApi.js
+++ front/src/services/parser/api/coreApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = `${window.location.origin}/core-alert-job/api/`;
+export const API_URL = `${window.location.origin}/core/api/`;
 
 const core_api = axios.create({
 	baseURL: API_URL,
