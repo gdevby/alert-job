649b2002800dec64b59ed1817e82732da39d6aec
robert <robert@work-1>
2023-01-12 17:39:27 +0300

    mapped native loc name

--- common-alert-job/src/main/java/by/gdev/common/model/CategoryDTO.java
+++ common-alert-job/src/main/java/by/gdev/common/model/CategoryDTO.java
@@ -4,9 +4,10 @@ import lombok.Data;
 
 @Data
 public class CategoryDTO {
-	
+
 	private Long id;
 	private String name;
+	private String nativeLocName;
 	private String link;
 	private boolean parse;
 
--- common-alert-job/src/main/java/by/gdev/common/model/SubCategoryDTO.java
+++ common-alert-job/src/main/java/by/gdev/common/model/SubCategoryDTO.java
@@ -4,9 +4,10 @@ import lombok.Data;
 
 @Data
 public class SubCategoryDTO {
-	
+
 	private Long id;
 	private String name;
+	private String nativeLocName;
 	private String link;
 	private boolean parse;
 
