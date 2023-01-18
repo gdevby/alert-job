55655eaa65374b65f711cb9fcff0e0d98c4156d6
Daniel <jasp_m_lv@mail.ru>
2023-01-18 17:55:49 +0300

    improved slice

--- front/src/store/slices/filterSlice.js
+++ front/src/store/slices/filterSlice.js
@@ -22,12 +22,23 @@ const filterSlice = createSlice({
 		setCurrentFilter(state, action) {
 			state.currentFilter.descriptionWords = action.payload.description
 			state.currentFilter.titleWords = action.payload.title
-			state.currentFilter.technologyWords = action.payload.technology
+			state.currentFilter.technologyWords = action.payload.technologies
 			state.currentFilter.name = action.payload.name
 			state.currentFilter.id = action.payload.id
 			state.currentFilter.maxPrice = action.payload.maxPrice
 			state.currentFilter.minPrice = action.payload.minPrice
 			state.isChoose = true
+		},
+		removeCurrentFilter(state, action) {
+			state.currentFilter.descriptionWords = []
+			state.currentFilter.titleWords = []
+			state.currentFilter.technologyWords = []
+			state.currentFilter.name = ''
+			state.currentFilter.id = null
+			state.currentFilter.maxPrice = 0
+			state.currentFilter.minPrice = 0
+			state.isChoose = false
+			state.isNew = true
 		},
 		setFilters(state, action) {
 			state.filters = action.payload.filters
@@ -37,10 +48,28 @@ const filterSlice = createSlice({
 		},
 		setIsNew(state, action) {
 			state.isNew = action.payload.isNew
+		},
+		changeTechologies(state, action) {
+			state.currentFilter.technologyWords = [...state.currentFilter.technologyWords, action.payload.technologies]
+		},
+		changeTitles(state, action) {
+			state.currentFilter.titleWords = [...state.currentFilter.titleWords, action.payload.titles]
+		},
+		changeDescriptions(state, action) {
+			state.currentFilter.descriptionWords = [...state.currentFilter.descriptionWords, action.payload.decscriptions]
+		},
+		changeFilterName(state, action) {
+			state.currentFilter.name = action.payload.name
+		},
+		changeMaxPrice(state, action) {
+			state.currentFilter.maxPrice = action.payload.maxPrice
+		},
+		changeMinPrice(state, action) {
+			state.currentFilter.minPrice = action.payload.minPrice
 		}
 	},
 });
 
-export const { setCurrentFilter, setFilters, removeFilter, setIsNew } = filterSlice.actions;
+export const { setCurrentFilter, setFilters, removeFilter, setIsNew, removeCurrentFilter } = filterSlice.actions;
 
 export default filterSlice.reducer;
