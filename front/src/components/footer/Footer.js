90ce969ccc0d23363a24788197774358f1f16a25
Daniel <jasp_m_lv@mail.ru>
2023-01-19 14:44:15 +0300

    imporved footer

--- front/src/components/footer/Footer.js
+++ front/src/components/footer/Footer.js
@@ -3,7 +3,14 @@ import React from 'react'
 import './footer.scss'
 
 const Footer = () => {
-	return <div className='footer'>footer</div>
+	return <div className='footer'>
+			<div className='container'>
+				<div className='footer_content'>
+					<b>Это открый проект</b> <a href='https://github.com/gdevby/alert-job'>https://github.com/gdevby/alert-job</a>
+				</div>
+			</div>
+
+		</div>
 }
 
 export default Footer
