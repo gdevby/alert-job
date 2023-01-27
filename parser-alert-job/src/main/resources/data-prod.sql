6ff1272db708a13534a52f33af19b78d9f669797
robert <robert@work-1>
2023-01-27 17:42:42 +0300

    added for prod

---
+++ parser-alert-job/src/main/resources/data-prod.sql
@@ -0,0 +1,24 @@
+             
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(1, 'Менеджмент');          
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(2, 'Разработка сайтов');           
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(3, 'Дизайн и Арт');        
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(5, 'Программирование');            
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(6, 'Оптимизация (SEO)');               
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(7, 'Переводы');    
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(8, 'Тексты');              
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(9, '3D Графика');      
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(10, 'Фотография');         
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(11, 'Аудио/Видео');        
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(12, 'Реклама и Маркетинг');    
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(13, 'Аутсорсинг и консалтинг');
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(14, 'Архитектура/Интерьер');           
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(16, 'Разработка игр');         
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(17, 'Полиграфия');         
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(19, 'Анимация и флеш');        
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(20, 'Инжиниринг');         
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(22, 'Обучение и консультации');
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(23, 'Мобильные приложения');           
+INSERT INTO fl_category(ID,NATIVE_LOC_NAME) VALUES(24, 'Сети и инфосистемы');          
+       
+INSERT INTO site_source_job(id,name,parseduri,parse) VALUES(1, 'FLRU', 'https://www.fl.ru/projects/', TRUE); 
+INSERT INTO site_source_job(id,name,parseduri,parse) VALUES(2, 'HABR', 'https://freelance.habr.com/tasks', TRUE);            
