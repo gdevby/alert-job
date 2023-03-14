de836b0c182b5b6e44ebfc43177488f6847bfe8a
Daniel <jasp_m_lv@mail.ru>
2023-03-14 11:39:14 +0300

    added mobile menu

--- front/src/App.js
+++ front/src/App.js
@@ -3,14 +3,16 @@ import React from 'react'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Router from './components/router/Router'
+import { CssBaseline } from '@mui/material'
 
 import './index.scss'
 
 const App = () => {
 
 
-
+//<CssBaseline/>
 	return <>
+		
 		<div className='wrapper'>
 			<div className='top'>
 				<Header />
