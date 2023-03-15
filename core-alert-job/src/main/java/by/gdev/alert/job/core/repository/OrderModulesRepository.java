d7c0a13824c25fb42fe7c37111fbcecc533453c7
alexander <alexander@alexander-desktop>
2023-03-15 15:05:53 +0300

    fixed remove filter

--- core-alert-job/src/main/java/by/gdev/alert/job/core/repository/OrderModulesRepository.java
+++ core-alert-job/src/main/java/by/gdev/alert/job/core/repository/OrderModulesRepository.java
@@ -24,11 +24,4 @@ public interface OrderModulesRepository extends CrudRepository<OrderModules, Lon
 	
 	@Query("select o from OrderModules o left join fetch o.currentFilter left join fetch o.user u where o.id = :id and u.uuid = :uuid")
 	Optional<OrderModules> findByIdAndUserUuidOneEagerCurrentFilter(Long id, String uuid);
-	
-	
-	@Query("select o from OrderModules o left join fetch o.filters f left join fetch o.user u where o.id = :id and u.uuid = :uuid and f.id = :fid")
-	Optional<OrderModules> findByIdAndUserUuidOneEagerFilters(Long id, String uuid, Long fid);
-	
-	
-	
 }
