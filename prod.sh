09f1b862a6b604a7db410b097cc4245b3fa568ee
robert <robert@work-1>
2023-03-16 17:42:23 +0300

    improved prod

--- prod.sh
+++ prod.sh
@@ -1,19 +1,24 @@
+#every times when you change some parameters you need to recreate images of the docker ./prod.sh create
+export elasticsearch_directory=/home/app/service/aj/files_prod/elasticsearch
+export database_directory=/home/app/service/aj/files_prod/apps
+export path_to_docker_compose_prod_yml=/home/app/service/aj/sources/alert-job/docker-compose-prod.yml
 export database_user_password=test
 export database_user_login=test
 #used this param by default, after change, cas another can't be worked
 export keycloak_admin_password=admin
 export keycloak_admin=admin
-export local_config_directory=/home/ubuntu/alert-job-config-repo
+#local configuration files
+export local_config_directory=/home/app/service/aj/sources/alert-job-config-repo
 export keycloak_url=https://auth.gdev.by
-export configuration_override_parameters_directory_docker=/alert-job-config-repo
 export telegram_chat_token=
 export mail_login=
 export mail_password=
-export parser_categories_file_path=
-export swag_config=
-export front_directory=
-export logstash_config_directory=
+#path to locale DIRECTORY which contains this file https://github.com/gdevby/alert-job/blob/main/parser-alert-job/src/main/resources/hubr.txt 
+export parser_config_directory=/home/app/service/aj/parser
+#you can run swag and after changed files swag/nginx/site-confs/default.conf to configurated resolving services
+export swag_config_directory=/home/app/service/aj/swag
+export front_directory=/home/app/service/aj/sources/alert-job/front
+export logstash_config_directory=/home/app/service/aj/sources/alert-job/logstash/config
 export logger_level=INFO
 export kibana_secret_path=
-
-docker compose -f docker-compose-prod.yml $1 $2
+docker compose -f path_to_docker_compose_prod_yml $1 $2
