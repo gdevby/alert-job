11a747de98e16cd94833f4f8c485e3184d0be3e3
alexander <alexander@alexander-desktop>
2023-03-20 13:56:41 +0300

    changed query

--- core-alert-job/src/main/java/by/gdev/alert/job/core/repository/TitleWordRepository.java
+++ core-alert-job/src/main/java/by/gdev/alert/job/core/repository/TitleWordRepository.java
@@ -17,9 +17,9 @@ public interface TitleWordRepository extends CrudRepository<TitleWord, Long>{
 	
 	Optional<TitleWord> findByNameAndSourceSite(String name, SourceSite sourceSite);
 	
-	@Query("select t from TitleWord t where t.sourceSite IN :sourceSite or t.uuid =:uuid order by t.counter desc")
+	@Query("select t from TitleWord t where t.sourceSite.id IN :sourceSite or t.uuid =:uuid order by t.counter desc")
 	Page<TitleWord> findByNameAndSourceSiteInOrUuid(String uuid, Set<Long> sourceSite, Pageable p);
 	
-	@Query("select t from TitleWord t where (t.name = :name and t.sourceSite IN :sourceSite) or (t.name = :name and t.uuid =:uuid) order by t.counter desc")
+	@Query("select t from TitleWord t where (t.name = :name and t.sourceSite.id IN :sourceSite) or (t.name = :name and t.uuid =:uuid) order by t.counter desc")
 	Page<TitleWord> findByNameAndSourceSiteInOrNameAndUuid(String name, String uuid, Set<Long> sourceSite, Pageable p);
 }
