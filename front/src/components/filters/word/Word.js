f8f8503218b179cdc4a04ae8a95c1a06170efd74
Daniel <jasp_m_lv@mail.ru>
2023-03-21 16:24:39 +0300

    improved words

--- front/src/components/filters/word/Word.js
+++ front/src/components/filters/word/Word.js
@@ -1,16 +1,17 @@
 import React from 'react'
 
 import ClearIcon from '@mui/icons-material/Clear';
+import Item from '../../common/item/Item'
 
 const Word = ({ item, remove }) => {
 	const removeItem = () => {
 		remove(item.id)
 	}
 
-	return <div className='word'>
+	return <Item className='word'>
 		<div className='word__name'>{item.name}</div>
 		<div className='word__remove' onClick={removeItem} ><ClearIcon /></div>
-	</div>
+	</Item>
 }
 
 export default React.memo(Word)
