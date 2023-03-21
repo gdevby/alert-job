e84fe83ab7f94553aea994707fb04006c8efe324
robert <robert@work-1>
2023-03-21 18:29:49 +0300

    added @UniqueConstraint

--- core-alert-job/src/main/java/by/gdev/alert/job/core/model/db/SourceSite.java
+++ core-alert-job/src/main/java/by/gdev/alert/job/core/model/db/SourceSite.java
@@ -1,6 +1,8 @@
 package by.gdev.alert.job.core.model.db;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -8,6 +10,7 @@ import lombok.EqualsAndHashCode;
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
+@Table(uniqueConstraints = { @UniqueConstraint(name = "site_source_q_key", columnNames = { "siteSource","siteCategory","siteSubCategory" }) })
 public class SourceSite extends BasicId {
     private Long siteSource;
     private Long siteCategory;
