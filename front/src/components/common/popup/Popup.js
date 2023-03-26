97f7aa086dec3a287735779f3e04df7005c5de6d
Daniel <jasp_m_lv@mail.ru>
2023-03-26 16:16:11 +0300

    implemented popup

---
+++ front/src/components/common/popup/Popup.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '../button/Button'
+
+const Popup = ({ open, handleClose, title, content }) => {
+	return <Dialog
+		open={open}
+		onClose={handleClose}
+		aria-labelledby="alert-dialog-title"
+		aria-describedby="alert-dialog-description"
+	>
+		<DialogTitle id="alert-dialog-title">
+			{title}
+		</DialogTitle>
+		<DialogContent>
+			<DialogContentText id="alert-dialog-description">
+				{content}
+			</DialogContentText>
+		</DialogContent>
+		<DialogActions>
+			<Button onClick={handleClose} text={'Закрыть'} />
+		</DialogActions>
+	</Dialog>
+}
+
+export default Popup
