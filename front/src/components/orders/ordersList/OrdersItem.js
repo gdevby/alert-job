3fbf69d45d0efc9fad5fc7810ae2ea45bbe9f012
Daniel <jasp_m_lv@mail.ru>
2023-03-26 16:20:00 +0300

    fixed

--- front/src/components/orders/ordersList/OrdersItem.js
+++ front/src/components/orders/ordersList/OrdersItem.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { useResize } from '../../../hooks/useResize';
 
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -7,12 +8,8 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MobileOrderItemHead from '../../mobile/mobileOrderItem/MobileOrderItemHead';
 import OrdersItemHead from '../ordersItem/OrdersItemHead';
-import { useResize } from '../../../hooks/useResize';
-
 
 const OrdersItem = ({ order }) => {
-//625
-	
 	const { width } = useResize()
 
 	useEffect(() => {
@@ -24,8 +21,6 @@ const OrdersItem = ({ order }) => {
 			{link}
 		</Typography>
 	}
-
-	//<MobileOrderItemHead />
 
 	return <Accordion>
 		<AccordionSummary
--- front/src/components/orders/ordersList/OrdersList.js
+++ front/src/components/orders/ordersList/OrdersList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import OrdersItem from './OrdersItem'
 
 const OrdersList = ({ orders }) => {
