75f56d05f130e6f101b9bb15e8ee6c05e5cbe80f
robert <robert@work-1>
2023-04-06 11:19:07 +0300

    fixed item is empty

--- parser-alert-job/src/main/java/by/gdev/alert/job/parser/service/FreelanceRuOrderParser.java
+++ parser-alert-job/src/main/java/by/gdev/alert/job/parser/service/FreelanceRuOrderParser.java
@@ -23,6 +23,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.google.common.collect.Lists;
+
 import by.gdev.alert.job.parser.domain.db.Category;
 import by.gdev.alert.job.parser.domain.db.Order;
 import by.gdev.alert.job.parser.domain.db.ParserSource;
@@ -77,36 +79,37 @@ public class FreelanceRuOrderParser extends AbsctractSiteParser {
 		JAXBContext jaxbContext = JAXBContext.newInstance(Rss.class);
 		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 		Rss rss = (Rss) jaxbUnmarshaller.unmarshal(new URL(rssURI));
-		return rss.getChannel().getItem().stream()
-				.filter(f -> service.isExistsOrder(category, subCategory, f.getLink())).map(m -> {
-					service.saveOrderLinks(category, subCategory, m.getLink());
-					Order order = new Order();
-					order.setTitle(m.getTitle());
-					order.setDateTime(m.getPubDate());
-					order.setLink(m.getLink());
-					order = parseExtraFields(order, m.getDescription());
-					ParserSource parserSource = new ParserSource();
-					parserSource.setSource(siteSourceJobId);
-					parserSource.setCategory(category.getId());
-					parserSource.setSubCategory(Objects.nonNull(subCategory) ? subCategory.getId() : null);
-					parserSource.setOpenForAll(order.isOpenForAll());
-					order.setSourceSite(parserSource);
-					return order;
-				}).filter(e -> e.isValidOrder()).map(m -> {
-					ParserSource parserSource = m.getSourceSite();
-					Optional<ParserSource> optionalSource = parserSourceRepository
-							.findBySourceAndCategoryAndSubCategory(parserSource.getSource(), parserSource.getCategory(),
-									parserSource.getSubCategory());
-					if (optionalSource.isPresent()) {
-						parserSource = optionalSource.get();
-					} else {
-						parserSource = parserSourceRepository.save(parserSource);
-					}
-					m.setSourceSite(parserSource);
-					m = orderRepository.save(m);
-					return mapper.map(m, OrderDTO.class);
-				}).peek(e -> log.debug("found new order {} {}", e.getTitle(), e.getLink()))
-				.collect(Collectors.toList());
+		return Objects.isNull(rss.getChannel()) ? Lists.newArrayList()
+				: rss.getChannel().getItem().stream()
+						.filter(f -> service.isExistsOrder(category, subCategory, f.getLink())).map(m -> {
+							service.saveOrderLinks(category, subCategory, m.getLink());
+							Order order = new Order();
+							order.setTitle(m.getTitle());
+							order.setDateTime(m.getPubDate());
+							order.setLink(m.getLink());
+							order = parseExtraFields(order, m.getDescription());
+							ParserSource parserSource = new ParserSource();
+							parserSource.setSource(siteSourceJobId);
+							parserSource.setCategory(category.getId());
+							parserSource.setSubCategory(Objects.nonNull(subCategory) ? subCategory.getId() : null);
+							parserSource.setOpenForAll(order.isOpenForAll());
+							order.setSourceSite(parserSource);
+							return order;
+						}).filter(e -> e.isValidOrder()).map(m -> {
+							ParserSource parserSource = m.getSourceSite();
+							Optional<ParserSource> optionalSource = parserSourceRepository
+									.findBySourceAndCategoryAndSubCategory(parserSource.getSource(),
+											parserSource.getCategory(), parserSource.getSubCategory());
+							if (optionalSource.isPresent()) {
+								parserSource = optionalSource.get();
+							} else {
+								parserSource = parserSourceRepository.save(parserSource);
+							}
+							m.setSourceSite(parserSource);
+							m = orderRepository.save(m);
+							return mapper.map(m, OrderDTO.class);
+						}).peek(e -> log.debug("found new order {} {}", e.getTitle(), e.getLink()))
+						.collect(Collectors.toList());
 	}
 
 	@SneakyThrows
