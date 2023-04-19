184d4863888a4a9f693d81b1daaa8d29b677e411
alexander <alexander@alexander-desktop>
2023-04-19 16:54:06 +0300

    added transactional timeout

--- parser-alert-job/src/main/java/by/gdev/alert/job/parser/service/FLOrderParser.java
+++ parser-alert-job/src/main/java/by/gdev/alert/job/parser/service/FLOrderParser.java
@@ -9,7 +9,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import javax.transaction.Transactional;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
@@ -18,6 +17,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.google.common.collect.Lists;
@@ -45,87 +45,87 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FLOrderParser extends AbsctractSiteParser {
 
-	private final WebClient webClient;
-	private final ParserService service;
-	private final SiteSourceJobRepository siteSourceJobRepository;
-	private final OrderRepository orderRepository;
-	private final ParserSourceRepository parserSourceRepository;
+    private final WebClient webClient;
+    private final ParserService service;
+    private final SiteSourceJobRepository siteSourceJobRepository;
+    private final OrderRepository orderRepository;
+    private final ParserSourceRepository parserSourceRepository;
 
-	private Pattern paymentPatter = Pattern.compile(".*[Бб]юджет: ([0-9]+).*");
-	private Pattern currencyPatter = Pattern.compile("[0-9].*&#8381;");
+    private Pattern paymentPatter = Pattern.compile(".*[Бб]юджет: ([0-9]+).*");
+    private Pattern currencyPatter = Pattern.compile("[0-9].*&#8381;");
 
-	private final ModelMapper mapper;
+    private final ModelMapper mapper;
 
-	@Transactional
-	public List<OrderDTO> flruParser() {
-		return super.getOrders(1L);
+    @Transactional(timeout = 200)
+    public List<OrderDTO> flruParser() {
+	return super.getOrders(1L);
+    }
+
+    @SneakyThrows
+    List<OrderDTO> mapItems(String rssURI, Long siteSourceJobId, Category category, Subcategory subCategory) {
+	JAXBContext jaxbContext = JAXBContext.newInstance(Rss.class);
+	Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+	Rss rss = (Rss) jaxbUnmarshaller.unmarshal(new URL(rssURI));
+	return Objects.isNull(rss.getChannel().getItem()) ? Lists.newArrayList()
+		: rss.getChannel().getItem().stream()
+			.filter(f -> service.isExistsOrder(category, subCategory, f.getLink())).map(m -> {
+			    service.saveOrderLinks(category, subCategory, m.getLink());
+			    Order order = new Order();
+			    order.setTitle(m.getTitle());
+			    order.setDateTime(m.getPubDate());
+			    order.setMessage(m.getDescription());
+			    order.setLink(m.getLink());
+			    order = parsePrice(order);
+			    String title = order.getTitle().replaceAll("(\\(Бюджет: .*[0-9\\;\\)])", "");
+			    order.setTitle(title);
+			    ParserSource parserSource = new ParserSource();
+			    parserSource.setSource(siteSourceJobId);
+			    parserSource.setCategory(category.getId());
+			    parserSource.setSubCategory(Objects.nonNull(subCategory) ? subCategory.getId() : null);
+			    order.setSourceSite(parserSource);
+			    return order;
+			}).filter(e -> e.isValidOrder()).map(m -> {
+			    ParserSource parserSource = m.getSourceSite();
+			    Optional<ParserSource> optionalSource = parserSourceRepository
+				    .findBySourceAndCategoryAndSubCategory(parserSource.getSource(),
+					    parserSource.getCategory(), parserSource.getSubCategory());
+			    if (optionalSource.isPresent()) {
+				parserSource = optionalSource.get();
+			    } else {
+				parserSource = parserSourceRepository.save(parserSource);
+			    }
+			    m.setSourceSite(parserSource);
+			    m = orderRepository.save(m);
+			    return mapper.map(m, OrderDTO.class);
+			}).peek(e -> log.debug("found new order {} {}", e.getTitle(), e.getLink()))
+			.collect(Collectors.toList());
+    }
+
+    @SneakyThrows
+    private Order parsePrice(Order order) {
+	Matcher m = paymentPatter.matcher(order.getTitle());
+	Price price = new Price();
+	if (m.find()) {
+	    price.setValue(Integer.valueOf(m.group(1)));
+	    order.setPrice(price);
 	}
-
-	@SneakyThrows
-	List<OrderDTO> mapItems(String rssURI, Long siteSourceJobId, Category category, Subcategory subCategory) {
-		JAXBContext jaxbContext = JAXBContext.newInstance(Rss.class);
-		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-		Rss rss = (Rss) jaxbUnmarshaller.unmarshal(new URL(rssURI));
-		return Objects.isNull(rss.getChannel().getItem()) ? Lists.newArrayList()
-				: rss.getChannel().getItem().stream()
-						.filter(f -> service.isExistsOrder(category, subCategory, f.getLink())).map(m -> {
-							service.saveOrderLinks(category, subCategory, m.getLink());
-							Order order = new Order();
-							order.setTitle(m.getTitle());
-							order.setDateTime(m.getPubDate());
-							order.setMessage(m.getDescription());
-							order.setLink(m.getLink());
-							order = parsePrice(order);
-							String title = order.getTitle().replaceAll("(\\(Бюджет: .*[0-9\\;\\)])", "");
-							order.setTitle(title);
-							ParserSource parserSource = new ParserSource();
-							parserSource.setSource(siteSourceJobId);
-							parserSource.setCategory(category.getId());
-							parserSource.setSubCategory(Objects.nonNull(subCategory) ? subCategory.getId() : null);
-							order.setSourceSite(parserSource);
-							return order;
-						}).filter(e -> e.isValidOrder()).map(m -> {
-							ParserSource parserSource = m.getSourceSite();
-							Optional<ParserSource> optionalSource = parserSourceRepository
-									.findBySourceAndCategoryAndSubCategory(parserSource.getSource(),
-											parserSource.getCategory(), parserSource.getSubCategory());
-							if (optionalSource.isPresent()) {
-								parserSource = optionalSource.get();
-							} else {
-								parserSource = parserSourceRepository.save(parserSource);
-							}
-							m.setSourceSite(parserSource);
-							m = orderRepository.save(m);
-							return mapper.map(m, OrderDTO.class);
-						}).peek(e -> log.debug("found new order {} {}", e.getTitle(), e.getLink()))
-						.collect(Collectors.toList());
+	Matcher m1 = currencyPatter.matcher(order.getTitle());
+	if (m1.find()) {
+	    price.setPrice(m1.group(0).replaceAll("&#8381;", "руб."));
+	    order.setPrice(price);
 	}
-
-	@SneakyThrows
-	private Order parsePrice(Order order) {
-		Matcher m = paymentPatter.matcher(order.getTitle());
-		Price price = new Price();
-		if (m.find()) {
-			price.setValue(Integer.valueOf(m.group(1)));
-			order.setPrice(price);
-		}
-		Matcher m1 = currencyPatter.matcher(order.getTitle());
-		if (m1.find()) {
-			price.setPrice(m1.group(0).replaceAll("&#8381;", "руб."));
-			order.setPrice(price);
-		}
-		Document doc = null;
-		try {
-			doc = Jsoup.parse(new URL(order.getLink()), 30000);
-		} catch (IOException ex) {
-			order.setValidOrder(false);
-			log.debug("invalid flru link " + order.getLink());
-			return order;
-		}
-		Element el = doc.selectFirst(".b-layout__txt_lineheight_1");
-		if (Objects.nonNull(el) && (el.text().contains("Срочный заказ") || el.text().contains("Для всех"))) {
-			order.setOpenForAll(true);
-		}
-		return order;
+	Document doc = null;
+	try {
+	    doc = Jsoup.parse(new URL(order.getLink()), 30000);
+	} catch (IOException ex) {
+	    order.setValidOrder(false);
+	    log.debug("invalid flru link " + order.getLink());
+	    return order;
 	}
+	Element el = doc.selectFirst(".b-layout__txt_lineheight_1");
+	if (Objects.nonNull(el) && (el.text().contains("Срочный заказ") || el.text().contains("Для всех"))) {
+	    order.setOpenForAll(true);
+	}
+	return order;
+    }
 }
--- parser-alert-job/src/main/java/by/gdev/alert/job/parser/service/HabrOrderParser.java
+++ parser-alert-job/src/main/java/by/gdev/alert/job/parser/service/HabrOrderParser.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import javax.transaction.Transactional;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
@@ -16,6 +15,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.google.common.collect.Lists;
@@ -43,74 +43,74 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HabrOrderParser extends AbsctractSiteParser {
 
-	private final WebClient webClient;
-	private final ParserService service;
-	private final SiteSourceJobRepository siteSourceJobRepository;
-	private final OrderRepository orderRepository;
-	private final ParserSourceRepository parserSourceRepository;
+    private final WebClient webClient;
+    private final ParserService service;
+    private final SiteSourceJobRepository siteSourceJobRepository;
+    private final OrderRepository orderRepository;
+    private final ParserSourceRepository parserSourceRepository;
 
-	private final ModelMapper mapper;
+    private final ModelMapper mapper;
 
-	@Transactional
-	public List<OrderDTO> hubrParser() {
-		return super.getOrders(2L);
+    @Transactional(timeout = 200)
+    public List<OrderDTO> hubrParser() {
+	return super.getOrders(2L);
+    }
+
+    @SneakyThrows
+    List<OrderDTO> mapItems(String rssURI, Long siteSourceJobId, Category category, Subcategory subCategory) {
+	JAXBContext jaxbContext = JAXBContext.newInstance(Rss.class);
+	Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+	Rss rss = (Rss) jaxbUnmarshaller.unmarshal(new URL(rssURI));
+	return Objects.isNull(rss.getChannel().getItem()) ? Lists.newArrayList()
+		: rss.getChannel().getItem().stream()
+			.filter(f -> service.isExistsOrder(category, subCategory, f.getLink())).map(m -> {
+			    service.saveOrderLinks(category, subCategory, m.getLink());
+			    Order order = new Order();
+			    order.setTitle(m.getTitle());
+			    order.setDateTime(m.getPubDate());
+			    order.setMessage(m.getDescription());
+			    order.setLink(m.getLink());
+			    order = parsePrice(order);
+			    ParserSource parserSource = new ParserSource();
+			    parserSource.setSource(siteSourceJobId);
+			    parserSource.setCategory(category.getId());
+			    parserSource.setSubCategory(Objects.nonNull(subCategory) ? subCategory.getId() : null);
+			    order.setSourceSite(parserSource);
+			    return order;
+			}).filter(e -> e.isValidOrder()).map(m -> {
+			    ParserSource parserSource = m.getSourceSite();
+			    Optional<ParserSource> optionalSource = parserSourceRepository
+				    .findBySourceAndCategoryAndSubCategory(parserSource.getSource(),
+					    parserSource.getCategory(), parserSource.getSubCategory());
+			    if (optionalSource.isPresent()) {
+				parserSource = optionalSource.get();
+			    } else {
+				parserSource = parserSourceRepository.save(parserSource);
+			    }
+			    m.setSourceSite(parserSource);
+			    m = orderRepository.save(m);
+			    return mapper.map(m, OrderDTO.class);
+			}).peek(e -> log.debug("found new order {} {}", e.getTitle(), e.getLink()))
+			.collect(Collectors.toList());
+    }
+
+    private Order parsePrice(Order order) {
+	Document doc = null;
+	try {
+	    doc = Jsoup.parse(new URL(order.getLink()), 30000);
+	    Element el = doc.selectFirst("span.count");
+	    Element elPaymentType = el.selectFirst(".suffix");
+	    if (Objects.nonNull(elPaymentType) && elPaymentType.text().equals("за проект")) {
+		String s = el.childNode(0).toString();
+		order.setPrice(new Price(s, Integer.valueOf(s.replaceAll(" ", "").replaceAll("руб.", ""))));
+	    }
+	    Elements elements = doc.select(".tags__item_link");
+	    order.setTechnologies(elements.eachText().stream().collect(Collectors.toList()));
+	} catch (Exception ex) {
+	    order.setValidOrder(false);
+	    log.debug("invalid hubr link " + order.getLink());
+	    return order;
 	}
-
-	@SneakyThrows
-	List<OrderDTO> mapItems(String rssURI, Long siteSourceJobId, Category category, Subcategory subCategory) {
-		JAXBContext jaxbContext = JAXBContext.newInstance(Rss.class);
-		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-		Rss rss = (Rss) jaxbUnmarshaller.unmarshal(new URL(rssURI));
-		return Objects.isNull(rss.getChannel().getItem()) ? Lists.newArrayList()
-				: rss.getChannel().getItem().stream()
-						.filter(f -> service.isExistsOrder(category, subCategory, f.getLink())).map(m -> {
-							service.saveOrderLinks(category, subCategory, m.getLink());
-							Order order = new Order();
-							order.setTitle(m.getTitle());
-							order.setDateTime(m.getPubDate());
-							order.setMessage(m.getDescription());
-							order.setLink(m.getLink());
-							order = parsePrice(order);
-							ParserSource parserSource = new ParserSource();
-							parserSource.setSource(siteSourceJobId);
-							parserSource.setCategory(category.getId());
-							parserSource.setSubCategory(Objects.nonNull(subCategory) ? subCategory.getId() : null);
-							order.setSourceSite(parserSource);
-							return order;
-						}).filter(e -> e.isValidOrder()).map(m -> {
-							ParserSource parserSource = m.getSourceSite();
-							Optional<ParserSource> optionalSource = parserSourceRepository
-									.findBySourceAndCategoryAndSubCategory(parserSource.getSource(),
-											parserSource.getCategory(), parserSource.getSubCategory());
-							if (optionalSource.isPresent()) {
-								parserSource = optionalSource.get();
-							} else {
-								parserSource = parserSourceRepository.save(parserSource);
-							}
-							m.setSourceSite(parserSource);
-							m = orderRepository.save(m);
-							return mapper.map(m, OrderDTO.class);
-						}).peek(e -> log.debug("found new order {} {}", e.getTitle(), e.getLink()))
-						.collect(Collectors.toList());
-	}
-
-	private Order parsePrice(Order order) {
-		Document doc = null;
-		try {
-			doc = Jsoup.parse(new URL(order.getLink()), 30000);
-			Element el = doc.selectFirst("span.count");
-			Element elPaymentType = el.selectFirst(".suffix");
-			if (Objects.nonNull(elPaymentType) && elPaymentType.text().equals("за проект")) {
-				String s = el.childNode(0).toString();
-				order.setPrice(new Price(s, Integer.valueOf(s.replaceAll(" ", "").replaceAll("руб.", ""))));
-			}
-			Elements elements = doc.select(".tags__item_link");
-			order.setTechnologies(elements.eachText().stream().collect(Collectors.toList()));
-		} catch (Exception ex) {
-			order.setValidOrder(false);
-			log.debug("invalid hubr link " + order.getLink());
-			return order;
-		}
-		return order;
-	}
+	return order;
+    }
 }
