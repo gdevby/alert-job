d33d1ea07073ee51078a98f0fa574f7d2d564ac8
alexander <alexander@alexander-desktop>
2023-06-08 09:52:20 +0300

    fixed problem with forbidden

---
+++ notification-alert-job/src/main/java/by/gdev/alert/job/notification/exeption/ErrorHandlingController.java
@@ -0,0 +1,22 @@
+package by.gdev.alert.job.notification.exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import by.gdev.common.exeption.ResponseExeption;
+
+@ControllerAdvice
+public class ErrorHandlingController {
+
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
+    @ExceptionHandler(WebClientResponseException.Forbidden.class)
+    @ResponseBody
+    public ResponseExeption webExchangeBindExceptionHandler(WebClientResponseException.Forbidden ex) {
+	return new ResponseExeption(ex.getMessage());
+    }
+
+}
--- notification-alert-job/src/main/java/by/gdev/alert/job/notification/service/MailService.java
+++ notification-alert-job/src/main/java/by/gdev/alert/job/notification/service/MailService.java
@@ -22,37 +22,30 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class MailService {
 
-	private final ApplicationProperty property;
-	private final Mailer mailer;
-	private final WebClient webClient;
+    private final ApplicationProperty property;
+    private final Mailer mailer;
+    private final WebClient webClient;
 
-	public Mono<Void> sendMessage(UserNotification userMail) {
-		return Mono.defer(() -> {
-			Email mail = EmailBuilder.startingBlank().from(property.getMailLogin()).to(userMail.getToMail())
-					.withSubject("Email").withHTMLText(userMail.getMessage()).buildEmail();
-			mailer.sendMail(mail);
-			return Mono.empty();
-		}).doOnSuccess(r -> log.info("sent message for user email {}, {}", userMail.getToMail(), userMail.getMessage()))
-				.doOnError(e -> log.info("can't send test message for user email  {}, {}", userMail.getToMail(),
-						userMail.getMessage()))
-				.then();
-	}
+    public Mono<Void> sendMessage(UserNotification userMail) {
+	return Mono.defer(() -> {
+	    Email mail = EmailBuilder.startingBlank().from(property.getMailLogin()).to(userMail.getToMail())
+		    .withSubject("Email").withHTMLText(userMail.getMessage()).buildEmail();
+	    mailer.sendMail(mail);
+	    return Mono.empty();
+	}).doOnSuccess(r -> log.info("sent message for user email {}, {}", userMail.getToMail(), userMail.getMessage()))
+		.doOnError(e -> log.info("can't send test message for user email  {}, {}", userMail.getToMail(),
+			userMail.getMessage()))
+		.then();
+    }
 
-	public Mono<Void> sendMessageToTelegram(UserNotification userMail) {
-		return Mono.defer(() -> {
-			MessageData m = new MessageData(Long.valueOf(userMail.getToMail()), userMail.getMessage());
-			webClient.post()
-					.uri("https://api.telegram.org",
-							u -> u.path("/bot{token}/sendMessage").build(property.getTelegramChatToken()))
-					.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).bodyValue(m).retrieve()
-					.toBodilessEntity()
-					.doOnSuccess(r -> log.info("sent message for user telegram {}, {}", userMail.getToMail(),
-							userMail.getMessage()))
-					.doOnError(e -> log.info("can't send test message for user telegram {}, {}", userMail.getToMail(),
-							userMail.getMessage()))
-					.subscribe();
-			return Mono.empty();
-		});
-	}
-
+    public Mono<Void> sendMessageToTelegram(UserNotification userMail) {
+	MessageData m = new MessageData(Long.valueOf(userMail.getToMail()), userMail.getMessage());
+	return webClient.post()
+		.uri("https://api.telegram.org",
+			u -> u.path("/bot{token}/sendMessage").build(property.getTelegramChatToken()))
+		.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).bodyValue(m).retrieve()
+		.bodyToMono(Void.class).doOnSuccess(r -> log.info("sent message for user telegram {}, {}",
+			userMail.getToMail(), userMail.getMessage()))
+		.onErrorResume(ex -> Mono.error(ex));
+    }
 }
