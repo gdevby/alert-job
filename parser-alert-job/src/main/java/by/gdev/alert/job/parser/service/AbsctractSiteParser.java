4bfcd360f285a2d43a1ae0bdaffbcf65f3c65b6f
alexander <alexander@alexander-desktop>
2023-08-07 10:08:41 +0300

    added catch UnmarshalException

--- parser-alert-job/src/main/java/by/gdev/alert/job/parser/service/AbsctractSiteParser.java
+++ parser-alert-job/src/main/java/by/gdev/alert/job/parser/service/AbsctractSiteParser.java
@@ -3,6 +3,8 @@ package by.gdev.alert.job.parser.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.UnmarshalException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import by.gdev.alert.job.parser.domain.db.Category;
@@ -14,45 +16,47 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public abstract class AbsctractSiteParser {
-	@Autowired
-	private SiteSourceJobRepository siteSourceJobRepository;
+    @Autowired
+    private SiteSourceJobRepository siteSourceJobRepository;
 
-	public List<OrderDTO> getOrders(Long siteId) {
+    public List<OrderDTO> getOrders(Long siteId) throws javax.xml.bind.UnmarshalException {
 
-		List<OrderDTO> orders = new ArrayList<>();
-		try {
-			SiteSourceJob siteSourceJob = siteSourceJobRepository.findById(siteId).get();
-			log.trace("parsed {}", siteSourceJob.getName());
-			siteSourceJob.getCategories().stream()
-					// parse only categories that can parse=true
-					// iterate over each category from this collection
-					.forEach(category -> {
-						log.trace("getting order by category {} rss link {}", category.getNativeLocName(),
-								category.getLink());
-						List<Subcategory> siteSubCategories = category.getSubCategories();
-						// checking if a subcategory exists for this category
-						// category does't have a subcategory
-						if (category.isParse())
-							orders.addAll(mapItems(category.getLink(), siteSourceJob.getId(), category, null));
-						// category have a subcategory
-						siteSubCategories.stream()
-								// parse only sub categories that can parse=true
-								.filter(subCategoryFilter -> subCategoryFilter.isParse())
-								// Iterate all sub category
-								.forEach(subCategory -> {
-									log.trace("getting order by category {} and subcategory  {} {}",
-											category.getNativeLocName(), subCategory.getNativeLocName(),
-											subCategory.getLink());
-									List<OrderDTO> list1 = mapItems(subCategory.getLink(), siteSourceJob.getId(),
-											category, subCategory);
-									orders.addAll(list1);
-								});
-					});
-		} catch (Exception e) {
-			log.error("error", e);
-		}
-		return orders;
+	List<OrderDTO> orders = new ArrayList<>();
+	try {
+	    SiteSourceJob siteSourceJob = siteSourceJobRepository.findById(siteId).get();
+	    log.trace("parsed {}", siteSourceJob.getName());
+	    siteSourceJob.getCategories().stream()
+		    // parse only categories that can parse=true
+		    // iterate over each category from this collection
+		    .forEach(category -> {
+			log.trace("getting order by category {} rss link {}", category.getNativeLocName(),
+				category.getLink());
+			List<Subcategory> siteSubCategories = category.getSubCategories();
+			// checking if a subcategory exists for this category
+			// category does't have a subcategory
+			if (category.isParse())
+			    orders.addAll(mapItems(category.getLink(), siteSourceJob.getId(), category, null));
+			// category have a subcategory
+			siteSubCategories.stream()
+				// parse only sub categories that can parse=true
+				.filter(subCategoryFilter -> subCategoryFilter.isParse())
+				// Iterate all sub category
+				.forEach(subCategory -> {
+				    log.trace("getting order by category {} and subcategory  {} {}",
+					    category.getNativeLocName(), subCategory.getNativeLocName(),
+					    subCategory.getLink());
+				    List<OrderDTO> list1 = mapItems(subCategory.getLink(), siteSourceJob.getId(),
+					    category, subCategory);
+				    orders.addAll(list1);
+				});
+		    });
+	} catch (Exception e) {
+	    if (e instanceof UnmarshalException)
+		log.warn("{Unmarshal problem }", e.getCause().getMessage());
+	    log.error("error", e);
 	}
+	return orders;
+    }
 
-	abstract List<OrderDTO> mapItems(String link, Long siteSourceJobId, Category c, Subcategory sub);
+    abstract List<OrderDTO> mapItems(String link, Long siteSourceJobId, Category c, Subcategory sub);
 }
