0918b561b2047117a799fb212c0dc8c38d492e6f
alexander <alexander@alexander-desktop>
2023-08-08 12:22:59 +0300

    removed static

--- parser-alert-job/src/main/java/by/gdev/alert/job/parser/util/ParserStringUtils.java
+++ parser-alert-job/src/main/java/by/gdev/alert/job/parser/util/ParserStringUtils.java
@@ -5,11 +5,11 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ParserStringUtils {
 
-    public static final String PROXY_CLIENT = "proxy_client";
-    public static final String COUNTER_HUBR = "counter_hubr";
-    public static final String COUNTER_FLRU = "counter_flru";
-    public static final String COUNTER_FREELANCERU = "counter_freelanceru";
-    public static final String COUNTER_WEBLANCER = "counter_weblancer";
-    public static final String COUNTER_FREELANCEHUN = "counter_freelancehun";
+    public final String PROXY_CLIENT = "proxy_client";
+    public final String COUNTER_HUBR = "counter_hubr";
+    public final String COUNTER_FLRU = "counter_flru";
+    public final String COUNTER_FREELANCERU = "counter_freelanceru";
+    public final String COUNTER_WEBLANCER = "counter_weblancer";
+    public final String COUNTER_FREELANCEHUN = "counter_freelancehun";
 
 }
