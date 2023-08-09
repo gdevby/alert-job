dfb98338a1fcc8edc89f40a4c02f802169c1e2a7
robert <robert@robert-desktop>
2023-08-09 16:11:12 +0300

    fixed error with word

--- parser-alert-job/src/main/java/by/gdev/alert/job/parser/controller/OrderParserController.java
+++ parser-alert-job/src/main/java/by/gdev/alert/job/parser/controller/OrderParserController.java
@@ -48,118 +48,118 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class OrderParserController {
 
-    public final HabrOrderParser hubr;
-    public final FLOrderParser fl;
-    public final FreelanceRuOrderParser freelanceRuOrderParser;
-    public final WeblancerOrderParcer weblancerOrderParcer;
-    public final FreelancehuntOrderParcer freelancehuntOrderParcer;
-    public final ParserService service;
+	public final HabrOrderParser hubr;
+	public final FLOrderParser fl;
+	public final FreelanceRuOrderParser freelanceRuOrderParser;
+	public final WeblancerOrderParcer weblancerOrderParcer;
+	public final FreelancehuntOrderParcer freelancehuntOrderParcer;
+	public final ParserService service;
 
-    @Value("${parser.interval}")
-    private long parserInterval;
+	@Value("${parser.interval}")
+	private long parserInterval;
 
-    private final ApplicationContext context;
-    private final MeterRegistry meterRegistry;
+	private final ApplicationContext context;
+	private final MeterRegistry meterRegistry;
 
-    @GetMapping(value = "/stream-sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<ServerSentEvent<List<OrderDTO>>> streamFlruEvents() {
-	log.trace("subscribed on orders");
-	Flux<ServerSentEvent<List<OrderDTO>>> flruFlux = Flux
-		.interval(Duration.ofSeconds(parserInterval)).map(sequence -> ServerSentEvent.<List<OrderDTO>>builder()
-			.id(String.valueOf(sequence)).event("periodic-flru-parse-event").data(fl.flruParser()).build())
-		.doOnNext(s -> {
-		    int size = s.data().size();
-		    context.getBean(COUNTER_FLRU, Counter.class).increment(size);
-		});
-	Flux<ServerSentEvent<List<OrderDTO>>> hubrFlux = Flux.interval(Duration.ofSeconds(parserInterval))
-		.map(sequence -> ServerSentEvent.<List<OrderDTO>>builder().id(String.valueOf(sequence))
-			.event("periodic-hubr-parse-event").data(hubr.hubrParser()).build())
-		.doOnNext(s -> {
-		    int size = s.data().size();
-		    context.getBean(COUNTER_HUBR, Counter.class).increment(size);
-		});
+	@GetMapping(value = "/stream-sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<ServerSentEvent<List<OrderDTO>>> streamFlruEvents() {
+		log.trace("subscribed on orders");
+		Flux<ServerSentEvent<List<OrderDTO>>> flruFlux = Flux
+				.interval(Duration.ofSeconds(parserInterval)).map(sequence -> ServerSentEvent.<List<OrderDTO>>builder()
+						.id(String.valueOf(sequence)).event("periodic-flru-parse-event").data(fl.flruParser()).build())
+				.doOnNext(s -> {
+					int size = s.data().size();
+					context.getBean(COUNTER_FLRU, Counter.class).increment(size);
+				});
+		Flux<ServerSentEvent<List<OrderDTO>>> hubrFlux = Flux.interval(Duration.ofSeconds(parserInterval))
+				.map(sequence -> ServerSentEvent.<List<OrderDTO>>builder().id(String.valueOf(sequence))
+						.event("periodic-hubr-parse-event").data(hubr.hubrParser()).build())
+				.doOnNext(s -> {
+					int size = s.data().size();
+					context.getBean(COUNTER_HUBR, Counter.class).increment(size);
+				});
 
-	Flux<ServerSentEvent<List<OrderDTO>>> freelanceRuFlux = Flux.interval(Duration.ofSeconds(parserInterval))
-		.map(sequence -> ServerSentEvent.<List<OrderDTO>>builder().id(String.valueOf(sequence))
-			.event("periodic-freelanceru-parse-event").data(freelanceRuOrderParser.getOrders()).build())
-		.doOnNext(s -> {
-		    int size = s.data().size();
-		    context.getBean(COUNTER_FREELANCERU, Counter.class).increment(size);
-		});
-	Flux<ServerSentEvent<List<OrderDTO>>> weblancerFlux = Flux.interval(Duration.ofSeconds(parserInterval))
-		.map(sequence -> ServerSentEvent.<List<OrderDTO>>builder().id(String.valueOf(sequence))
-			.event("periodic-weblancer-parse-event").data(weblancerOrderParcer.weblancerParser()).build())
-		.doOnNext(s -> {
-		    int size = s.data().size();
-		    context.getBean(COUNTER_WEBLANCER, Counter.class).increment(size);
-		});
-	Flux<ServerSentEvent<List<OrderDTO>>> freelancehuntOrderParcerFlux = Flux
-		.interval(Duration.ofSeconds(parserInterval))
-		.map(sequence -> ServerSentEvent.<List<OrderDTO>>builder().id(String.valueOf(sequence))
-			.event("periodic-freelancehun-parse-event").data(freelancehuntOrderParcer.freelancehuntParser())
-			.build())
-		.doOnNext(s -> {
-		    int size = s.data().size();
-		    context.getBean(COUNTER_FREELANCEHUNT, Counter.class).increment(size);
-		});
-	return Flux.merge(flruFlux, hubrFlux, freelanceRuFlux, weblancerFlux, freelancehuntOrderParcerFlux);
-    }
+		Flux<ServerSentEvent<List<OrderDTO>>> freelanceRuFlux = Flux.interval(Duration.ofSeconds(parserInterval))
+				.map(sequence -> ServerSentEvent.<List<OrderDTO>>builder().id(String.valueOf(sequence))
+						.event("periodic-freelanceru-parse-event").data(freelanceRuOrderParser.getOrders()).build())
+				.doOnNext(s -> {
+					int size = s.data().size();
+					context.getBean(COUNTER_FREELANCERU, Counter.class).increment(size);
+				});
+		Flux<ServerSentEvent<List<OrderDTO>>> weblancerFlux = Flux.interval(Duration.ofSeconds(parserInterval))
+				.map(sequence -> ServerSentEvent.<List<OrderDTO>>builder().id(String.valueOf(sequence))
+						.event("periodic-weblancer-parse-event").data(weblancerOrderParcer.weblancerParser()).build())
+				.doOnNext(s -> {
+					int size = s.data().size();
+					context.getBean(COUNTER_WEBLANCER, Counter.class).increment(size);
+				});
+		Flux<ServerSentEvent<List<OrderDTO>>> freelancehuntOrderParcerFlux = Flux
+				.interval(Duration.ofSeconds(parserInterval))
+				.map(sequence -> ServerSentEvent.<List<OrderDTO>>builder().id(String.valueOf(sequence))
+						.event("periodic-freelancehunt-parse-event")
+						.data(freelancehuntOrderParcer.freelancehuntParser()).build())
+				.doOnNext(s -> {
+					int size = s.data().size();
+					context.getBean(COUNTER_FREELANCEHUNT, Counter.class).increment(size);
+				});
+		return Flux.merge(flruFlux, hubrFlux, freelanceRuFlux, weblancerFlux, freelancehuntOrderParcerFlux);
+	}
 
-    @GetMapping("sites")
-    public Flux<SiteSourceDTO> sites() {
-	return service.getSites();
-    }
+	@GetMapping("sites")
+	public Flux<SiteSourceDTO> sites() {
+		return service.getSites();
+	}
 
-    @GetMapping("categories")
-    public Flux<CategoryDTO> categories(@RequestParam("site_id") Long site) {
-	return service.getCategories(site);
-    }
+	@GetMapping("categories")
+	public Flux<CategoryDTO> categories(@RequestParam("site_id") Long site) {
+		return service.getCategories(site);
+	}
 
-    @GetMapping("subcategories")
-    public Flux<SubCategoryDTO> subCategories(@RequestParam("category_id") Long category) {
-	return service.getSubCategories(category);
-    }
+	@GetMapping("subcategories")
+	public Flux<SubCategoryDTO> subCategories(@RequestParam("category_id") Long category) {
+		return service.getSubCategories(category);
+	}
 
-    @GetMapping("site/{id}")
-    public Mono<SiteSourceDTO> site(@PathVariable("id") Long id) {
-	return service.getSite(id);
-    }
+	@GetMapping("site/{id}")
+	public Mono<SiteSourceDTO> site(@PathVariable("id") Long id) {
+		return service.getSite(id);
+	}
 
-    @GetMapping("site/{id}/category/{category_id}")
-    public Mono<CategoryDTO> category(@PathVariable("id") Long id, @PathVariable("category_id") Long cId) {
-	return service.getCategory(id, cId);
-    }
+	@GetMapping("site/{id}/category/{category_id}")
+	public Mono<CategoryDTO> category(@PathVariable("id") Long id, @PathVariable("category_id") Long cId) {
+		return service.getCategory(id, cId);
+	}
 
-    @GetMapping("category/{id}/subcategory/{sub_id}")
-    public Mono<SubCategoryDTO> subCategory(@PathVariable("id") Long id, @PathVariable("sub_id") Long subId) {
-	return service.getSubCategory(id, subId);
-    }
+	@GetMapping("category/{id}/subcategory/{sub_id}")
+	public Mono<SubCategoryDTO> subCategory(@PathVariable("id") Long id, @PathVariable("sub_id") Long subId) {
+		return service.getSubCategory(id, subId);
+	}
 
-    @PatchMapping("subscribe/sources")
-    public Mono<Void> subscribeSources(@RequestParam("category_id") Long categoryId,
-	    @RequestParam(name = "subcategory_id", required = false) Long subCategoryId,
-	    @RequestParam("category_value") boolean cValue,
-	    @RequestParam(name = "subcategory_value", required = false) boolean sValue) {
-	return service.subcribeOnSource(categoryId, subCategoryId, cValue, sValue);
-    }
+	@PatchMapping("subscribe/sources")
+	public Mono<Void> subscribeSources(@RequestParam("category_id") Long categoryId,
+			@RequestParam(name = "subcategory_id", required = false) Long subCategoryId,
+			@RequestParam("category_value") boolean cValue,
+			@RequestParam(name = "subcategory_value", required = false) boolean sValue) {
+		return service.subcribeOnSource(categoryId, subCategoryId, cValue, sValue);
+	}
 
-    @GetMapping("orders")
-    public Flux<OrderDTO> showOrdersBySource(@RequestParam("site_id") Long site,
-	    @RequestParam("category_id") Long category, @RequestParam(name = "sub_id", required = false) Long subId,
-	    @RequestParam("period") Long period) {
-	return service.getOrdersBySource(site, category, subId, period);
-    }
+	@GetMapping("orders")
+	public Flux<OrderDTO> showOrdersBySource(@RequestParam("site_id") Long site,
+			@RequestParam("category_id") Long category, @RequestParam(name = "sub_id", required = false) Long subId,
+			@RequestParam("period") Long period) {
+		return service.getOrdersBySource(site, category, subId, period);
+	}
 
-    @PostConstruct
-    void init() {
-	ConfigurableListableBeanFactory beanFactory = ((ConfigurableApplicationContext) context).getBeanFactory();
-	beanFactory.registerSingleton(COUNTER_HUBR, meterRegistry.counter(PROXY_CLIENT, PROXY_CLIENT, "hubr"));
-	beanFactory.registerSingleton(COUNTER_FLRU, meterRegistry.counter(PROXY_CLIENT, PROXY_CLIENT, "flru"));
-	beanFactory.registerSingleton(COUNTER_FREELANCERU,
-		meterRegistry.counter(PROXY_CLIENT, PROXY_CLIENT, "freelanceru"));
-	beanFactory.registerSingleton(COUNTER_WEBLANCER,
-		meterRegistry.counter(PROXY_CLIENT, PROXY_CLIENT, "weblancer"));
-	beanFactory.registerSingleton(COUNTER_FREELANCEHUNT,
-		meterRegistry.counter(PROXY_CLIENT, PROXY_CLIENT, "freelancehun"));
-    }
+	@PostConstruct
+	void init() {
+		ConfigurableListableBeanFactory beanFactory = ((ConfigurableApplicationContext) context).getBeanFactory();
+		beanFactory.registerSingleton(COUNTER_HUBR, meterRegistry.counter(PROXY_CLIENT, PROXY_CLIENT, "hubr"));
+		beanFactory.registerSingleton(COUNTER_FLRU, meterRegistry.counter(PROXY_CLIENT, PROXY_CLIENT, "flru"));
+		beanFactory.registerSingleton(COUNTER_FREELANCERU,
+				meterRegistry.counter(PROXY_CLIENT, PROXY_CLIENT, "freelanceru"));
+		beanFactory.registerSingleton(COUNTER_WEBLANCER,
+				meterRegistry.counter(PROXY_CLIENT, PROXY_CLIENT, "weblancer"));
+		beanFactory.registerSingleton(COUNTER_FREELANCEHUNT,
+				meterRegistry.counter(PROXY_CLIENT, PROXY_CLIENT, "freelancehun"));
+	}
 }
