6888e85873d5c648dd7849ae76139bc86fee3115
robert <robert@robert-desktop>
2023-08-09 15:37:07 +0300

    fixed problem with static

--- parser-alert-job/src/main/java/by/gdev/alert/job/parser/util/ParserStringUtils.java
+++ parser-alert-job/src/main/java/by/gdev/alert/job/parser/util/ParserStringUtils.java
@@ -1,15 +1,12 @@
 package by.gdev.alert.job.parser.util;
 
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
 public class ParserStringUtils {
-	public String PROXY_CLIENT = "proxy_client";
-	public String COUNTER_HUBR = "counter_hubr";
-	public String COUNTER_FLRU = "counter_flru";
-	public String COUNTER_FREELANCERU = "counter_freelanceru";
-	public String COUNTER_WEBLANCER = "counter_weblancer";
-	public String COUNTER_FREELANCEHUNT = "counter_freelancehunt";
-	public String VALUE = "value";
-	public String DUPBLICATE_KEY = "Duplicate key %s";
+	public static final String PROXY_CLIENT = "proxy_client";
+	public static final String COUNTER_HUBR = "counter_hubr";
+	public static final String COUNTER_FLRU = "counter_flru";
+	public static final String COUNTER_FREELANCERU = "counter_freelanceru";
+	public static final String COUNTER_WEBLANCER = "counter_weblancer";
+	public static final String COUNTER_FREELANCEHUNT = "counter_freelancehunt";
+	public static final String VALUE = "value";
+	public static final String DUPBLICATE_KEY = "Duplicate key %s";
 }
