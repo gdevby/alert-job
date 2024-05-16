4af0d2387b38a31ecf8b9db3a0646bfa22e1f959
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2024-05-16 12:14:36 +0300

    used unique keys

--- front/src/components/orders/ordersList/OrdersList.js
+++ front/src/components/orders/ordersList/OrdersList.js
@@ -2,10 +2,8 @@ import React from 'react'
 import OrdersItem from './OrdersItem'
 
 const OrdersList = ({ orders }) => {
-
-
 	return <div className={'orders__list'}>
-		{orders.map(item => <OrdersItem key={item.dateTime} order={item} />)}
+		{orders.map(item => <OrdersItem key={item.link} order={item} />)}
 	</div>
 }
 
