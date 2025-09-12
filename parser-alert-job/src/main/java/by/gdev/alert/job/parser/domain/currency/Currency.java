197fd06e7b86e1eff90009ac7e521de8d930c33b
ElReni <86531070+ElReni@users.noreply.github.com>
2025-09-12 14:08:43 +0300

    Added BOB currency

--- parser-alert-job/src/main/java/by/gdev/alert/job/parser/domain/currency/Currency.java
+++ parser-alert-job/src/main/java/by/gdev/alert/job/parser/domain/currency/Currency.java
@@ -2,5 +2,5 @@ package by.gdev.alert.job.parser.domain.currency;
 
 public enum Currency {
 	AUD, AZN, GBP, AMD, BYN, BGN, BRL, HUF, VND, HKD, GEL, DKK, AED, USD, EUR, EGP, INR, IDR, KZT, CAD, QAR, KGS, CNY,
-	MDL, NZD, NOK, PLN, RON, XDR, SGD, TJS, THB, TRY, TMT, UZS, UAH, CZK, SEK, CHF, RSD, ZAR, KRW, JPY, DZD, BHD;
+	MDL, NZD, NOK, PLN, RON, XDR, SGD, TJS, THB, TRY, TMT, UZS, UAH, CZK, SEK, CHF, RSD, ZAR, KRW, JPY, DZD, BHD, BOB;
 }
