fd79d1e2e64fe4c741d6d0d9c319a24794dbc2c4
ElReni <ElReni@host.docker.internal>
2025-09-22 18:14:51 +0300

    Added new currencies

--- parser-alert-job/src/main/java/by/gdev/alert/job/parser/domain/currency/Currency.java
+++ parser-alert-job/src/main/java/by/gdev/alert/job/parser/domain/currency/Currency.java
@@ -2,5 +2,6 @@ package by.gdev.alert.job.parser.domain.currency;
 
 public enum Currency {
 	AUD, AZN, GBP, AMD, BYN, BGN, BRL, HUF, VND, HKD, GEL, DKK, AED, USD, EUR, EGP, INR, IDR, KZT, CAD, QAR, KGS, CNY,
-	MDL, NZD, NOK, PLN, RON, XDR, SGD, TJS, THB, TRY, TMT, UZS, UAH, CZK, SEK, CHF, RSD, ZAR, KRW, JPY, DZD, BHD, BOB;
+	MDL, NZD, NOK, PLN, RON, XDR, SGD, TJS, THB, TRY, TMT, UZS, UAH, CZK, SEK, CHF, RSD, ZAR, KRW, JPY, DZD, BHD, BOB,
+	IRR, CUP, MNT, NGN, OMR, SAR, BDT, ETB, MMK;
 }
