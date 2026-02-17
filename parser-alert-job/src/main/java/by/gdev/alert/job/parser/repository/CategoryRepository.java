ab6852839fa7a9a7a9c45cdfd80348d8055091a4
provodnik_new <provodnik_new@mail.ru>
2026-02-17 12:01:09 +0300

    Remove Delete methor for CategoryRepository

--- parser-alert-job/src/main/java/by/gdev/alert/job/parser/repository/CategoryRepository.java
+++ parser-alert-job/src/main/java/by/gdev/alert/job/parser/repository/CategoryRepository.java
@@ -20,9 +20,4 @@ public interface CategoryRepository extends CrudRepository<Category, Long> {
 
 	@Query("select c from parser_category c left join fetch c.siteSourceJob s where c.id = :id and s.id = :sid")
 	Optional<Category> findByIdAndSourceId(@Param("id") Long id, @Param("sid") Long sid);
-
-    @Modifying
-    @Query("delete from parser_category c where c.siteSourceJob.id = :siteId")
-    void deleteAllBySourceId(@Param("siteId") Long siteId);
-
 }
