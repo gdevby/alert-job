c175b345ef97cecb984beb95e10588820bb09e5e
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2026-06-17 15:09:04 +0300

    fix: turn off modulePreload

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({ filename: 'bundle-visualizer.html' }),
-  ],
+  plugins: [react(), visualizer({ filename: 'bundle-visualizer.html' })],
   server: { port: 3000, strictPort: true, allowedHosts: true },
   resolve: {
     mainFields: [],
+  },
+  build: {
+    modulePreload: false,
   },
 });
