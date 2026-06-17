f266a6cd59139640493dd0c9f8f175a494216b10
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2026-06-17 17:44:59 +0300

    fix: fix "Uncaught TypeError: r.inherits is not a function" error

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { join } from 'node:path';
 
 export default defineConfig({
   plugins: [react(), visualizer({ filename: 'bundle-visualizer.html' })],
   server: { port: 3000, strictPort: true, allowedHosts: true },
+  preview: {
+    port: 3000,
+  },
   resolve: {
     mainFields: [],
+    alias: {
+      axios: join(__dirname, 'node_modules', 'axios', 'dist', 'esm', 'axios.js'),
+    }
   },
   build: {
     modulePreload: false,
