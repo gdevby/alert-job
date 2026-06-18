43b089ce2ffed8eb67ba60f48f1b64cdcc4c191e
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2026-06-18 16:54:59 +0300

    feat: use REVISION variable

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,33 +1,39 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { join } from 'node:path';
 
-export default defineConfig({
-  plugins: [react(), visualizer({ filename: 'bundle-visualizer.html' })],
-  server: { port: 3001, strictPort: true, allowedHosts: true },
-  preview: {
-    port: 3001,
-  },
-  resolve: {
-    mainFields: [],
-    alias: {
-      axios: join(__dirname, 'node_modules', 'axios', 'dist', 'esm', 'axios.js'),
-    }
-  },
-  build: {
-    outDir: 'dist/front',
-    modulePreload: false,
-    rolldownOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        assetFileNames: ({ names }) => {
-          if (names[0] === 'index.css') {
-            return '[name][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const REVISION = env.VITE_REVISION;
+
+  return {
+    plugins: [react(), visualizer({ filename: 'bundle-visualizer.html' })],
+    server: { port: 3001, strictPort: true, allowedHosts: true },
+    preview: {
+      port: 3001,
+    },
+    resolve: {
+      mainFields: [],
+      alias: {
+        axios: join(__dirname, 'node_modules', 'axios', 'dist', 'esm', 'axios.js'),
+      },
+    },
+    build: {
+      /* for vite preview need comment outDir property */
+      outDir: 'dist/front',
+      modulePreload: false,
+      rolldownOptions: {
+        output: {
+          entryFileNames: `[name]-v${REVISION}.js`,
+          assetFileNames: ({ names }) => {
+            if (names[0] === 'index.css') {
+              return `[name]-v${REVISION}[extname]`;
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
         },
       },
     },
-  },
+  };
 });
