b8a1ad45e74a639c49774584fabf1640b19b13e8
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2026-06-22 15:47:18 +0300

    Revert "fix: remove REVISION variable (#154)" (#156)
    This reverts commit 96dbbeac2990163cb7f93dead87fc486dc3bd6f2.

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { join } from 'node:path';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const REVISION = env.VITE_REVISION;
+
   return {
     plugins: [react(), visualizer({ filename: 'bundle-visualizer.html' })],
     server: { port: 3001, strictPort: true, allowedHosts: true },
@@ -22,10 +25,10 @@ export default defineConfig(() => {
       modulePreload: false,
       rolldownOptions: {
         output: {
-          entryFileNames: `[name].js`,
+          entryFileNames: `[name]-v${REVISION}.js`,
           assetFileNames: ({ names }) => {
             if (names[0] === 'index.css') {
-              return `[name][extname]`;
+              return `[name]-v${REVISION}[extname]`;
             }
             return 'assets/[name]-[hash][extname]';
           },
