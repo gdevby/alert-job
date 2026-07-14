4498585175802c7134acd3a26fb5c30713d1bc0a
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2026-07-14 18:23:02 +0300

    feat: create template via dialog

--- front/src/modules/auto-replies/components/TemplatesTab/TemplateDialog.tsx
+++ front/src/modules/auto-replies/components/TemplatesTab/TemplateDialog.tsx
@@ -1,8 +1,18 @@
 import './templateDialog.scss';
+
+import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 import TextField from '@mui/material/TextField';
 import DialogContent from '@mui/material/DialogContent';
+import type { FormState } from '@/lib/constants/FormState';
+import { Button } from '@mui/material';
+import { TemplatesApi, type TemplateRequest } from '@/apis/llmApi';
+
+const templatesApi = new TemplatesApi();
+
+type Fields = Pick<TemplateRequest, 'name' | 'text'>;
 
 type Props = {
   isOpen: boolean;
@@ -10,16 +20,51 @@ type Props = {
 };
 
 export const TemplateDialog = ({ isOpen, close }: Props) => {
+  const queryClient = useQueryClient();
+  const [name, setName] = useState<string>();
+  const [text, setText] = useState<string>();
+
+  const { mutate: createOrUpdateTemplate } = useMutation({
+    mutationFn: (data: TemplateRequest) => templatesApi.createOrUpdate(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['templatesApi.getTemplatesByUser'] });
+      close();
+    },
+    onError: () => {},
+  });
+
   const handleClose = () => {
     close();
   };
 
+  const handleCreateOrUpdateButton = () => {
+    createOrUpdateTemplate({ name, text });
+  };
+
   return (
     <Dialog className="template-dialog" fullWidth maxWidth="lg" open={isOpen} onClose={handleClose}>
-      <DialogTitle>Создание шаблона</DialogTitle>
+      <DialogTitle>Добавление шаблона</DialogTitle>
       <DialogContent className="template-dialog__content">
-        <TextField type="text" label="Название" variant="standard" placeholder="Название" />
-        <TextField label="Текст шаблона" placeholder="Текст шаблона" multiline rows={11} maxRows={100} />
+        <TextField
+          type="text"
+          label="Название"
+          variant="standard"
+          placeholder="Название"
+          onChange={e => setName(e.target.value)}
+        />
+        <TextField
+          label="Текст шаблона"
+          placeholder="Текст шаблона"
+          multiline
+          rows={16}
+          onChange={e => setText(e.target.value)}
+        />
+
+        <div className="template-dialog__submit-button">
+          <Button variant="contained" onClick={handleCreateOrUpdateButton}>
+            Создать
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
--- front/src/modules/auto-replies/components/TemplatesTab/TemplatesTab.tsx
+++ front/src/modules/auto-replies/components/TemplatesTab/TemplatesTab.tsx
@@ -43,7 +43,7 @@ export const TemplatesTab = () => {
 
   return (
     <div className="templates-tab">
-      <Button onClick={handleCreateTemplateButton}>Создать шаблон</Button>
+      <Button onClick={handleCreateTemplateButton}>Добавить шаблон</Button>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
