56d5b5d6fac836b5506df1bdaa13dbe68b9e8ec6
provodnik_new <provodnik_new@mail.ru>
2026-08-25 11:44:12 +0300

    Add log in AiDecisionWorker

--- notification-alert-job/src/main/java/by/gdev/alert/job/notification/service/ai/queue/AiDecisionWorker.java
+++ notification-alert-job/src/main/java/by/gdev/alert/job/notification/service/ai/queue/AiDecisionWorker.java
@@ -45,43 +45,47 @@ public class AiDecisionWorker {
         DecryptedCredential creds = null;
 
         for (StepType type : PIPELINE) {
+            String userUuid = payload.getUser() != null ? payload.getUser().getUuid() : "unknown";
             log.info("АВТООТВЕТ: этап {} -> НАЧАЛО", type);
             switch (type) {
                 case RESOLVE_SITE -> {
                     var step = (AiStep<AiNotificationPayload, SiteName>) stepMap.get(type);
                     var r = step.execute(payload);
-                    if (r.failed()) return;
+                    if (r.failed()) {
+                        log.warn("АВТООТВЕТ: этап {} -> ОШИБКА, пользователь: {}", type, userUuid);
+                        return;
+                    }
                     site = r.value();
-                    log.debug("АВТООТВЕТ: этап {} -> сайт определен: {}", type, site);
+                    log.debug("АВТООТВЕТ: этап {} -> сайт определен: {}, пользователь: {}", type, site, userUuid);
                 }
                 case GET_PARSER -> {
                     var step = (AiStep<SiteName, AutoreplyPlaywrightParser>) stepMap.get(type);
                     var r = step.execute(site);
                     if (r.failed()) return;
                     parser = r.value();
-                    log.info("АВТООТВЕТ: этап {} -> парсер получен: {}", type, parser.getSiteName());
+                    log.info("АВТООТВЕТ: этап {} -> парсер получен: {}, пользователь: {}", type, parser.getSiteName(), userUuid);;
                 }
                 case GET_CREDENTIALS -> {
                     var step = (AiStep<AiNotificationPayload, DecryptedCredential>) stepMap.get(type);
                     var r = step.execute(payload);
                     if (r.failed()) return;
                     creds = r.value();
-                    log.info("АВТООТВЕТ: этап {} -> учетные данные получены для пользователя: {}", type, creds.login());
+                    log.info("АВТООТВЕТ: этап {} -> учетные данные получены для пользователя: {}, пользователь: {}", type, creds.login(), userUuid);
                 }
                 case SEND_AUTOREPLY -> {
                     var step = (AiStep<SendAutoreplyInput, Boolean>) stepMap.get(type);
                     var r = step.execute(new SendAutoreplyInput(parser, creds, payload));
                     if (r.failed()) {
-                        log.warn("АВТООТВЕТ: этап {} -> ОШИБКА при отправке автоответа", type);
+                        log.warn("АВТООТВЕТ: этап {} -> ОШИБКА при отправке автоответа, пользователь: {}", type, userUuid);
                         return;
                     } else {
-                        log.info("АВТООТВЕТ: этап {} -> автоответ отправлен успешно", type);
+                        log.info("АВТООТВЕТ: этап {} -> автоответ отправлен успешно, пользователь: {}", type, userUuid);
                     }
                 }
                 case SEND_NOTIFICATION -> {
                     var step = (AiStep<AiNotificationPayload, Void>) stepMap.get(type);
                     step.execute(payload);
-                    log.info("АВТООТВЕТ: этап {} -> уведомление отправлено", type);
+                    log.info("АВТООТВЕТ: этап {} -> уведомление отправлено, пользователь: {}", type, userUuid);
                 }
             }
         }
